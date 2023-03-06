9e928fd8f5964ad25363f74021e19b3e0a4f6338
HugoLelly <lellysoueu.hg@gmail.com>
2023-03-06 20:28:31 -0300

    commit 20230306 - 2028 (Fim da lista)

--- Aula 01/Aula 01 - Listas/Lista_09.py
+++ Aula 01/Aula 01 - Listas/Lista_09.py
@@ -6,8 +6,11 @@
 
 # 09) Criar uma lista de números inteiros e calcular a média dos números na lista.
 
-
-
-
 print(f'\n\t Universidade de Vassouras - Maricá \n\t Estrutura de Dados - Márcio Garrido \n\t 202211182 - Hugo Lelly de Lima Marinho')
 print(f'\n\t 09) Criar uma lista de números inteiros e calcular a média dos números na lista.')
+
+lista = [9, 4, 23, 7,8, 92, 3, 11]
+
+media = sum(lista) / len(lista)
+
+print(f'\n\t Na Lista {lista} a média é {media}')
--- Aula 01/Aula 01 - Listas/Lista_10.py
+++ Aula 01/Aula 01 - Listas/Lista_10.py
@@ -6,8 +6,9 @@
 
 # 10) Criar uma lista com nomes de pessoas e imprimir o primeiro e o último nome da lista.
 
-
-
-
 print(f'\n\t Universidade de Vassouras - Maricá \n\t Estrutura de Dados - Márcio Garrido \n\t 202211182 - Hugo Lelly de Lima Marinho')
 print(f'\n\t 10) Criar uma lista com nomes de pessoas e imprimir o primeiro e o último nome da lista.')
+
+nomes = ['Hugo', 'Larissa', 'Lays', 'Nalva', 'Louis', 'Nebulosa']
+
+print(f'\n\t Na lista {nomes} \n\t O primeiro nome é {nomes[0]} \n\t O ultimo nome é {nomes[-1]}')
