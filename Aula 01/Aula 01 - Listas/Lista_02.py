e4afd21c7d310ed42cad074cc928cc3219fd4b6f
HugoLelly <lellysoueu.hg@gmail.com>
2023-03-06 19:49:31 -0300

    commit 20230306 - 1949

--- Aula 01/Aula 01 - Listas/Lista_02.py
+++ Aula 01/Aula 01 - Listas/Lista_02.py
@@ -6,8 +6,15 @@
 
 # 02) Criar duas listas de números inteiros e mesclá-las em uma terceira lista em ordem crescente.
 
-
-
-
 print(f'\n\t Universidade de Vassouras - Maricá \n\t Estrutura de Dados - Márcio Garrido \n\t 202211182 - Hugo Lelly de Lima Marinho')
 print(f'\n\t 02) Criar duas listas de números inteiros e mesclá-las em uma terceira lista em ordem crescente.')
+
+lista1 = [9, 4, 23, 7]
+
+lista2 = [8, 92, 3, 11]
+
+lista3 = lista1 + lista2
+
+lista3.sort()
+
+print(f'\n\t Lista em oredem crescente \n\t {lista3}')
--- Aula 01/Aula 01 - Listas/Lista_03.py
+++ Aula 01/Aula 01 - Listas/Lista_03.py
@@ -6,8 +6,11 @@
 
 # 03) Criar uma lista com nomes de pessoas e ordená-la alfabeticamente.
 
-
-
-
 print(f'\n\t Universidade de Vassouras - Maricá \n\t Estrutura de Dados - Márcio Garrido \n\t 202211182 - Hugo Lelly de Lima Marinho')
 print(f'\n\t 03) Criar uma lista com nomes de pessoas e ordená-la alfabeticamente.')
+
+nomes = ['Hugo', 'Larissa', 'Lays', 'Nalva', 'Louis', 'Nebulosa']
+
+nomes.sort()
+
+print(f'\n\t Os nomes em ordem alfabetica é:\n\t {nomes}')
