02bcb30b2fe9032e5e106d31fd464ce7f4a4a24f
HugoLelly <lellysoueu.hg@gmail.com>
2023-03-06 19:56:29 -0300

    commit 20230306 - 1957

--- Aula 01/Aula 01 - Listas/Lista_04.py
+++ Aula 01/Aula 01 - Listas/Lista_04.py
@@ -6,8 +6,13 @@
 
 # 04) Remover todas as ocorrências de um determinado número de uma lista de números inteiros.
 
-
-
-
 print(f'\n\t Universidade de Vassouras - Maricá \n\t Estrutura de Dados - Márcio Garrido \n\t 202211182 - Hugo Lelly de Lima Marinho')
 print(f'\n\t 04) Remover todas as ocorrências de um determinado número de uma lista de números inteiros.')
+
+numeros = [1,2,1,4,1,3,1,5,6,1,9,1]
+
+remover = 1
+
+removidos = [n for n in numeros if n != remover]
+
+print(f'\n\t Removido o numero 1 da seguinte lista:\n\t {numeros} \n\t O resultado foi: \n\t {removidos}')
