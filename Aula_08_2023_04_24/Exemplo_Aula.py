9c925445135de15e4406acdf5f13fee9b9c66f4d
Hugo Lelly de Lima Marinho <lellysoueu.hg@gmail.com>
2023-04-24 21:48:43 -0300

    Atualização Aula 24/04/2023

---
+++ Aula_08_2023_04_24/Exemplo_Aula.py
@@ -0,0 +1,6 @@
+x = 1
+y = 2
+
+resultado = x + y
+
+print(resultado)
--- Aula_08_2023_04_24/Formas de Interpretação de Lógica/Exercicios_01_Formas_de_Interpretação_de_Logica.py
+++ Aula_08_2023_04_24/Formas de Interpretação de Lógica/Exercicios_01_Formas_de_Interpretação_de_Logica.py
@@ -9,4 +9,3 @@
 print(f'\n\tUniversidade de Vassouras - Maricá \n\tEstrutura de Dados - Márcio Garrido \n\t202211182 - Hugo Lelly de Lima Marinho')
 print(f'\n\t01) .')
 
-
